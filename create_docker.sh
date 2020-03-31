5c1d7adf7f412f435b6c61bb1ceaf122927b04db
Roy Novich <royno@mellanox.com>
2020-03-31 13:33:50 +0300

    create_docker: add repository flag
    added {-r|--repository} flag.
    give user ability for changing work git repository for docker
    users royno and valentinef have default path
    
    Signed-off-by: Roy Novich <royno@mellanox.com>

--- create_docker.sh
+++ create_docker.sh
@@ -47,16 +47,17 @@ do
 	esac
 	shift
 done
-#if [ -z "$repo_path" ]
-#then
-#	if [ "$whoami" = "valentinef" ] || [ "$whoami" = "royno"  ] 
-#		then
-#			repo_path="/swgwork/valentinef/rebase_5_1_backports/mlnx-ofa_kernel-4.0/"
-#		else
-#			echo "-E- 'create_docker.sh' must have full path to repository"
-#			exit 1
-#	fi
-#fi
+if [ -z "$repo_path" ]
+	then
+	if [[ "$(whoami)" == "valentinef" ]]; then
+	repo_path="/swgwork/valentinef/rebase_5_1_backports/mlnx-ofa_kernel-4.0/"
+	elif [[ "$(whoami)" == "royno" ]]; then
+	repo_path="/swgwork/royno/OFED_WORK_AREA/mlnx_ofed_5_1/mlnx-ofa_kernel-4.0/"
+	else
+	echo "-E- 'create_docker.sh' must have full path to repository [use {-r | --repository} flag]"
+	exit 1
+	fi
+fi
 if [ -z "$input_version" ]
 then
 	echo "-E- 'create_docker.sh' must have version [use: 'create_docker.sh -l' to see supported kernels]"
@@ -67,8 +68,8 @@ else
 	echo "---------------------------------------------------------------------------------------------"
 	echo "  use: 'source output/init_docker.sh'  ['source output/init_docker.sh -h' for help]"
 	echo "---------------------------------------------------------------------------------------------"
-	cp /swgwork/royno/OFED/my_scripts/init_docker.sh /tmp/output/
-sudo docker run -it --rm --entrypoint=/bin/bash --tmpfs /build:rw,exec,nosuid,mode=755,size=20G --mount type=tmpfs,target=/tmp/ -v /swgwork/royno/OFED_WORK_AREA/mlnx_ofed_5_1/mlnx-ofa_kernel-4.0/.git:/git-repo/:ro -v /tmp/output:/output -v /.autodirect/mswg2/work/kernel.org/x86_64/${input_version}/:/tmp/${input_version}/ harbor.mellanox.com/sw-linux-devops/cross_compile:latest 	
+	sudo cp /swgwork/royno/OFED/my_scripts/init_docker.sh /tmp/output/
+sudo docker run -it --rm --entrypoint=/bin/bash --tmpfs /build:rw,exec,nosuid,mode=755,size=20G --mount type=tmpfs,target=/tmp/ -v ${repo_path}.git:/git-repo/:ro -v /tmp/output:/output -v /.autodirect/mswg2/work/kernel.org/x86_64/${input_version}/:/tmp/${input_version}/ harbor.mellanox.com/sw-linux-devops/cross_compile:latest 	
 
 fi
 
