50e6d47f6ad616af7e2531e7a14ce8318d2d4b8a
Roy Novich <royno@nvidia.com>
2020-11-12 10:38:58 +0200

    get_ofed_basecode: initial commit
    Signed-off-by: Roy Novich <royno@nvidia.com>

---
+++ unifdef_tool/get_ofed_basecode.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+# Copyright (c) 2020 Mellanox Technologies. All rights reserved.
+#
+# This Software is licensed under one of the following licenses:
+#
+# 1) under the terms of the "Common Public License 1.0" a copy of which is
+#    available from the Open Source Initiative, see
+#    http://www.opensource.org/licenses/cpl.php.
+#
+# 2) under the terms of the "The BSD License" a copy of which is
+#    available from the Open Source Initiative, see
+#    http://www.opensource.org/licenses/bsd-license.php.
+#
+# 3) under the terms of the "GNU General Public License (GPL) Version 2" a
+#    copy of which is available from the Open Source Initiative, see
+#    http://www.opensource.org/licenses/gpl-license.php.
+#
+# Licensee has the right to choose one of the above licenses.
+#
+# Redistributions of source code must retain the above copyright
+# notice and one of the license notices.
+#
+# Redistributions in binary form must reproduce both the above copyright
+# notice, one of the license notices in the documentation
+# and/or other materials provided with the distribution.
+#
+# Author: Roy Novich <royno@nvidia.com>
+#
+# IMPORTANT: This script must be sourced to get full functionality
+
+ofa=$(\ls /usr/src | grep mlnx-ofa_kernel)
+ofa_dir=/usr/src/$ofa/
+CONFIG=/tmp/final_config.h
+NEW_DIR="/var/tmp/${ofa}_basecode"
+
+echo "script running.."
+echo $ofa
+if [ "X" == "X$ofa" ]; then
+	echo "-E- No Kernel src found, Aborting.." >&2
+	return 1
+fi
+
+echo "Copy src to $NEW_DIR"
+/bin/cp -rf $ofa_dir $NEW_DIR
+cd $NEW_DIR
+echo "Inside $PWD"
+if [ ! -f "compat/config.h" ]; then
+	echo "Configuring ofed envaironment"
+	sudo /swgwork/royno/OFED/my_scripts/configure_ofed_env
+fi
+sudo chown -f ${whoami} -R  $NEW_DIR
+sudo chown -f ${whoami} compat/config.h
+echo "Configure Done"
+echo "Create config file"
+/swgwork/royno/OFED/my_scripts/unifdef_tool/split_config_h.sh $PWD/compat/config.h
+/swgwork/royno/OFED/my_scripts/unifdef_tool/handle_config_h.sh /tmp/config.h
+/swgwork/royno/OFED/my_scripts/unifdef_tool/handle_configure_ac.sh /tmp/configure.ac
+
+if [ ! -f "${CONFIG}" ]; then
+	echo "-E- Config file does not exist at ${CONFIG}" >&2
+	return 1
+fi
+echo "start cleaning files.."
+for i in $(find ${PWD} \( -name '*.c' -o \
+			  -name '*.h' -o \
+			  -name 'Kbuild' -o \
+			  -name 'Makefile' \) )
+do
+	echo "cleaning ${i} ..."
+	sudo unifdef -f ${CONFIG} ${i} -o ${i}.tmp -b
+	mv -f ${i}.tmp $i
+done
