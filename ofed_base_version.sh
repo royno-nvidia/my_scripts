0a81067d21c5eeb9297d974bd4add85a5f0bf218
Roy Novich <royno@nvidia.com>
2021-05-04 08:12:33 +0300

    ofed_base_version: Check if branch exist
    Signed-off-by: Roy Novich <royno@nvidia.com>

--- ofed_base_version.sh
+++ ofed_base_version.sh
@@ -7,6 +7,12 @@ OFED_VER=$2
 CHECKOUT_BRANCH="mlnx_ofed_${OFED_VER}"
 cd $SRC_DIR
 CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+IS_BRANCH_EXIST=$(git ls-remote --heads origin ${CHECKOUT_BRANCH})
+if [ -z "$IS_BRANCH_EXIST" ]; then
+	echo
+	echo "-E- origin/${CHECKOUT_BRANCH} not exist.. abort"
+	exit 1
+fi
 git checkout --quiet "origin/${CHECKOUT_BRANCH}"
 BASE_VER=$(git log --oneline | grep -i "set base code" | grep -oE "v[0-9]\.[0-9]+.*")
 echo
