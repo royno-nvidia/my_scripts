fc2881b86a66c1d0171c7901077ac01c482a4e06
Roy Novich <royno@nvidia.com>
2021-06-24 09:27:20 +0300

    find_patches_by_status: Add feature and patch name each
    Signed-off-by: Roy Novich <royno@nvidia.com>

--- find_patches_by_status.sh
+++ find_patches_by_status.sh
@@ -7,6 +7,7 @@ TO=""
 STATUS=""
 OLD_IFS=$IFS
 IFS='\n'
+FORMAT_DIR=""
 ######### ARGS PARSER ########
 while [ ! -z "$1" ]
 do
@@ -21,6 +22,7 @@ do
 		-f, --from-hash		Start search hash.
 		-t, --to-hash		End search hash.
 		-s, --status		Status script looks for {accepted, ignore, NA, rejected, in_progress}
+		--format-patch		format-patch to given directory
 "
 		exit 1
 		;;
@@ -36,6 +38,14 @@ do
 			shift
 			STATUS=$1
 		;;
+		--format-patch)
+			shift
+			FORMAT_DIR=$1
+			if [ -z "$FORMAT_DIR" ];then
+				echo "-E- --format-patch missing path to dir, Aborting"
+				exit 1
+			fi
+		;;
 		*)
 		echo "-E- Unsupported option: $1" >&2
 		echo "use -h flag to display help menu"
@@ -50,17 +60,37 @@ if [ -z "$TO" ] || [ -z "$FROM" ]; then
 	echo "-E- must get -f, -t arguments, aborting.."
 	exit 1
 fi
-OUTPUT="/tmp/commit_status_${FROM}_${TO}.txt"
-echo "" > $OUTPUT
-git log --oneline --pretty='format:%s' $FROM^..$TO > $LOG
-while read sub; do
-	if (git grep "$sub" metadata/ | grep -q "upstream_status=$STATUS;" ); then
-		echo "$sub" >> $OUTPUT
+OUTPUTFILE="/tmp/commit_status_${FROM}_${TO}.csv"
+echo "sep=;">$OUTPUTFILE
+echo "Sha;Subject;Feature;Status;Note;Signed-off by;Patch name;" >>$OUTPUTFILE
+
+git log --oneline --pretty='format:%h;%s' $FROM..$TO > $LOG
+while read line; do
+	sha=$(echo "$line" | cut -d";" -f1)
+	sub=$(echo "$line" | cut -d";" -f2)
+	feature="$(git grep "$sub" | grep -oE "feature=.*;" | cut -d";" -f1 | sed 's/feature=//' | head -1)"
+	pname=""
+	echo "---------------------------------------------------"
+	echo "Working on: $sha $sub"
+	echo "feature: $feature"
+	if (git grep "subject=${sub}" metadata/ | grep -q "upstream_status=$STATUS;" ); then
+		echo "Metadata: $(git grep "subject=$sub" metadata/)"
+		if [ ! -z "$FORMAT_DIR" ]; then
+			ret=$(git format-patch -1 "$sha" -o "$FORMAT_DIR")
+			pname=$(echo $ret | sed 's/.*\///')
+			echo "Format-patch for commit at: $ret"
+			echo "pname: $pname"
+		fi
+		echo "$sha;$sub;$feature;;;;$pname;" >> $OUTPUTFILE
 	fi
+	echo "---------------------------------------------------"
 done < $LOG
 rm -rf $LOG
 IFS=$OLD_IFS
 echo
 echo "Script finished.."
-echo "Please see results in '$OUTPUT'"
+echo "Please see results in '$OUTPUTFILE'"
+if [ ! -z "$FORMAT_DIR" ]; then
+	echo "Format patches wait at: '$FORMAT_DIR'"
+fi
 exit 0
