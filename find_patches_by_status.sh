cf16f24da47ab35578d24d9b2f3de8bfa6d864ac
Roy Novich <royno@nvidia.com>
2021-06-29 13:57:13 +0300

    find_patches_by_status: Use moshe apply script and get status for patch
    Signed-off-by: Roy Novich <royno@nvidia.com>

--- find_patches_by_status.sh
+++ find_patches_by_status.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 ######## VARIABLES ########
 LOG="/tmp/log.txt"
@@ -6,8 +6,9 @@ FROM=""
 TO=""
 STATUS=""
 OLD_IFS=$IFS
-IFS='\n'
+#$IFS='\n'
 FORMAT_DIR=""
+APPLY_DIR=""
 ######### ARGS PARSER ########
 while [ ! -z "$1" ]
 do
@@ -21,8 +22,9 @@ do
 		-h, --help 		display this help message and exit.
 		-f, --from-hash		Start search hash.
 		-t, --to-hash		End search hash.
-		-s, --status		Status script looks for {accepted, ignore, NA, rejected, in_progress}
-		--format-patch		format-patch to given directory
+		-s, --status		Status script looks for {accepted, ignore, NA, rejected, in_progress}.
+		--format-patch		format-patch to given directory.
+		-a, --try-apply		Get accurate status for patch, need to get path for ofed dir where patch need to be applied.
 "
 		exit 1
 		;;
@@ -40,9 +42,17 @@ do
 		;;
 		--format-patch)
 			shift
-			FORMAT_DIR=$1
+			FORMAT_DIR="$1"
 			if [ -z "$FORMAT_DIR" ];then
 				echo "-E- --format-patch missing path to dir, Aborting"
+				exit 1
+			fi
+		;;
+		-a | --try_apply)
+			shift
+			APPLY_DIR="$1"
+			if [ -z "$APPLY_DIR" ];then
+				echo "-E- -a|--try_apply missing path to dir, Aborting"
 				exit 1
 			fi
 		;;
@@ -55,12 +65,38 @@ do
 	shift
 done
 
-######################
+########FUNCTIONS##########
+
+
+try_reverse_apply()
+{
+	local ofed_loc=$1;shift
+        local ploc=$1; shift
+	cwd="$(pwd)"	
+	cd "$ofed_loc"
+	#echo "*** git apply -R --check ${ploc}"
+	git apply -R --check --exclude="metadata/*" --exclude="backports/*" "${ploc}" 2> /dev/null
+	if [ $? -eq 0 ]; then
+		OUT_STR="Code fully inside"
+	else
+		OUT_STR="Not fully inside -"
+		git apply --check --exclude="metadata/*" ${ploc} 2> /dev/null
+		if [ $? -eq 0 ]; then
+			OUT_STR="${OUT_STR} Apply succeded"
+		else
+			OUT_STR="${OUT_STR} Apply failed"
+		fi
+	fi
+	cd "${cwd}"
+	echo "$OUT_STR"
+}
+
+###########################
 if [ -z "$TO" ] || [ -z "$FROM" ]; then
 	echo "-E- must get -f, -t arguments, aborting.."
 	exit 1
 fi
-OUTPUTFILE="/tmp/commit_status_${FROM}_${TO}.csv"
+OUTPUTFILE="commits_status.csv"
 echo "sep=;">$OUTPUTFILE
 echo "Sha;Subject;Feature;Status;Note;Signed-off by;Patch name;" >>$OUTPUTFILE
 
@@ -70,18 +106,21 @@ while read line; do
 	sub=$(echo "$line" | cut -d";" -f2)
 	feature="$(git grep "$sub" | grep -oE "feature=.*;" | cut -d";" -f1 | sed 's/feature=//' | head -1)"
 	pname=""
+	stat=""
 	echo "---------------------------------------------------"
 	echo "Working on: $sha $sub"
 	echo "feature: $feature"
 	if (git grep "subject=${sub}" metadata/ | grep -q "upstream_status=$STATUS;" ); then
 		echo "Metadata: $(git grep "subject=$sub" metadata/)"
 		if [ ! -z "$FORMAT_DIR" ]; then
-			ret=$(git format-patch -1 "$sha" -o "$FORMAT_DIR")
-			pname=$(echo $ret | sed 's/.*\///')
+			ret="$(git format-patch -1 "$sha" -o "$FORMAT_DIR")"
+			pname="$(echo $ret | sed 's/.*\///')"
 			echo "Format-patch for commit at: $ret"
 			echo "pname: $pname"
+			stat="$(try_reverse_apply "$APPLY_DIR" "$ret")"
+			echo "stat = $stat"
 		fi
-		echo "$sha;$sub;$feature;;;;$pname;" >> $OUTPUTFILE
+		echo "$sha;$sub;$feature;$stat;;;$pname;" >> $OUTPUTFILE
 	fi
 	echo "---------------------------------------------------"
 done < $LOG
