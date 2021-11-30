5c3f2ea71efddfba51526b78b1100aee00a1bdf7
Roy Novich <royno@nvidia.com>
2021-11-30 11:30:35 +0200

    build_defs_file: Take mk.config defines
    Signed-off-by: Roy Novich <royno@nvidia.com>

--- unifdef_tool/build_defs_file.sh
+++ unifdef_tool/build_defs_file.sh
@@ -33,56 +33,48 @@ if [ -d "$1" ]; then
 	IS_DIR=1
 	WORK_DIR=$1
 	COMPAT_FILE=$WORK_DIR/compat/config.h
-	AUTOCONF_FILE=$WORK_DIR/include/generated/autoconf.h
-	AUTOCONF_PATH=/tmp/final_autoconf.h
 else
 	if [ -f "$1" ]; then
 	IS_DIR=0
 	COMPAT_FILE=$1
 	else
 		echo "-E- Argument 1 for script must be directory/file path"
+		exit 1
 	fi
 
 fi
+echo $COMPAT_FILE
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CONFIG_PATH=/tmp/config.h
 CONFIGURE_PATH=/tmp/configure.ac
+MK_PATH=${WORK_DIR}/configure.mk.kernel
 DEFSFILE=/tmp/defs_file.h
 FINAL=$2
 if [ -z "$FINAL" ];then
 	FINAL=/tmp/final_defs.h
 fi
 
-sudo rm -rf $CONFIG_PATH $CONFIGURE_PATH $DEFSFILE $AUTOCONF_PATH
+sudo rm -rf $CONFIG_PATH $CONFIGURE_PATH $DEFSFILE
 echo "start build compat file '$FINAL' for unifdef use"
-/swgwork/royno/OFED/my_scripts/unifdef_tool/unifdef_installer.sh
-if [ $? -ne 0 ];then
-	echo "Script failed.."
-	exit 1
-fi
 cp $COMPAT_FILE /tmp/$(date +%s)_$(basename $COMPAT_FILE)
 $SCRIPTS_DIR/split_config_h.sh $COMPAT_FILE
 $SCRIPTS_DIR/handle_config_h.sh $CONFIG_PATH
 $SCRIPTS_DIR/handle_configure_ac.sh $CONFIGURE_PATH
-if [ $IS_DIR -eq 1 ];then
-	$SCRIPTS_DIR/handle_autoconf_h.sh $AUTOCONF_FILE
-fi
 
 echo "/*-----------------------*/" > $FINAL
 echo "/* config.h defs section */" >> $FINAL
 echo "/*-----------------------*/" >> $FINAL
+echo "/*-----------------------*/" >> $DEFSFILE
+echo "/* configure.mk.kernel defs section */" >> $DEFSFILE
+echo "/*-----------------------*/" >> $DEFSFILE
+grep =y ${MK_PATH} | sort | uniq | sed -e 's/=y/ 1/' | sed -e 's/^/#define /' >> $DEFSFILE
+grep -E "=$"  ${MK_PATH} | sort | uniq | sed -e 's/=//' | sed -e 's/^/#undef /' >> $DEFSFILE
 cat $DEFSFILE >> $FINAL
 echo "/*---------------------------*/" >> $FINAL
 echo "/* configure.ac defs section */" >> $FINAL
 echo "/*---------------------------*/" >> $FINAL
 unifdef -f $DEFSFILE $CONFIGURE_PATH >> $FINAL
-if [ $IS_DIR -eq 1 ];then
-	echo "/*-------------------------*/" >> $FINAL
-	echo "/* autoconf.h defs section */" >> $FINAL
-	echo "/*-------------------------*/" >> $FINAL
-	cat $AUTOCONF_PATH >> $FINAL
-fi
 
-rm -rf $CONFIG_PATH $CONFIGURE_PATH $DEFSFILE $AUTOCONF_PATH
+rm -rf $CONFIG_PATH $CONFIGURE_PATH $DEFSFILE
 echo "'${FINAL}' created"
 
