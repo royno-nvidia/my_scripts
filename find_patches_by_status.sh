8b931ac45e8105e9aa3e1f74d118681568e3ba4e
Roy Novich <royno@nvidia.com>
2022-02-07 17:31:39 +0200

    find_patches_by_status: Upstream status use
    Signed-off-by: Roy Novich <royno@nvidia.com>

--- find_patches_by_status.sh
+++ find_patches_by_status.sh
@@ -9,6 +9,7 @@ OLD_IFS=$IFS
 #$IFS='\n'
 FORMAT_DIR=""
 APPLY_DIR=""
+ALL=0
 ######### ARGS PARSER ########
 while [ ! -z "$1" ]
 do
@@ -22,9 +23,9 @@ do
 		-h, --help 		display this help message and exit.
 		-f, --from-hash		Start search hash.
 		-t, --to-hash		End search hash.
-		-s, --status		Status script looks for {accepted, ignore, NA, rejected, in_progress}.
+		-s, --status		Status script looks for {accepted, ignore, NA, rejected, in_progress}, default is all.
 		--format-patch		format-patch to given directory.
-		-a, --try-apply		Get accurate status for patch, need to get path for ofed dir where patch need to be applied.
+		-a, --try-apply		Get accurate status for patch, need to get path for OFED/Upstream dir where patch need to be applied.
 "
 		exit 1
 		;;
@@ -48,7 +49,7 @@ do
 				exit 1
 			fi
 		;;
-		-a | --try_apply)
+		-a | --try-apply)
 			shift
 			APPLY_DIR="$1"
 			if [ -z "$APPLY_DIR" ];then
@@ -92,25 +93,34 @@ try_reverse_apply()
 }
 
 ###########################
-if [ -z "$TO" ] || [ -z "$FROM" ]; then
-	echo "-E- must get -f, -t arguments, aborting.."
+if ([ -z "$TO" ] && [ ! -z "$FROM" ]) || ([ -z "$FROM" ] && [ ! -z "$TO" ]); then
+	echo "-E- must use -t,-f arguments together, aborting.."
 	exit 1
+fi
+if [ -z "$FROM" ] && [ -z "$TO" ]; then
+	ALL=1
 fi
 OUTPUTFILE="commits_status.csv"
 echo "sep=;">$OUTPUTFILE
-echo "Sha;Subject;Feature;Status;Note;Signed-off by;Patch name;" >>$OUTPUTFILE
+echo "Sha;Subject;Feature;Upstream status;Status;Note;Signed-off by;Patch name;" >>$OUTPUTFILE
 
-git log --oneline --pretty='format:%h;%s' $FROM..$TO > $LOG
+if [ "$ALL" -eq 1 ]; then
+	git log --oneline --pretty='format:%h;%s' > $LOG
+else
+	git log --oneline --pretty='format:%h;%s' $FROM..$TO > $LOG
+fi
 while read line; do
 	sha=$(echo "$line" | cut -d";" -f1)
 	sub=$(echo "$line" | cut -d";" -f2)
-	feature="$(git grep "$sub" | grep -oE "feature=.*;" | cut -d";" -f1 | sed 's/feature=//' | head -1)"
+	meta_line=$(git grep "$sub")
+	feature="$(echo $meta_line | grep -oE "feature=.*;" | cut -d";" -f1 | sed 's/.*=//' | head -1)"
+	ustatus="$(echo $meta_line | grep -oE "status.*;" | cut -d";" -f1 | sed 's/.*=//' | head -1)"
 	pname=""
 	stat=""
 	echo "---------------------------------------------------"
 	echo "Working on: $sha $sub"
 	echo "feature: $feature"
-	if (git grep "subject=${sub}" metadata/ | grep -q "upstream_status=$STATUS;" ); then
+	if (git grep "subject=${sub}" metadata/ | grep -q "upstream_status=$STATUS" ); then
 		echo "Metadata: $(git grep "subject=$sub" metadata/)"
 		if [ ! -z "$FORMAT_DIR" ]; then
 			ret="$(git format-patch -1 "$sha" -o "$FORMAT_DIR")"
@@ -120,7 +130,7 @@ while read line; do
 			stat="$(try_reverse_apply "$APPLY_DIR" "$ret")"
 			echo "stat = $stat"
 		fi
-		echo "$sha;$sub;$feature;$stat;;;$pname;" >> $OUTPUTFILE
+		echo "$sha;$sub;$feature;$ustatus;$stat;;;$pname;" >> $OUTPUTFILE
 	fi
 	echo "---------------------------------------------------"
 done < $LOG
